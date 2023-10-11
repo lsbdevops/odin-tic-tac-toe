5657cd89710e3f62caa51bb25e0d6ff5aab025a0
lsbdevops <lsbdevops@gmail.com>
2023-10-12 08:27:16 +1100

    Add funcs for player creation and making a move
    1. Function added to create a player based on name and assigned symbol.
    2. Function added to game board to allow move to be made on the board
    by modifying the associated array element.
    3. Game controller function created, which will be used to control
       the flow of the game.

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+// IFFE to initiate game board and associated actions. 
 const gameBoard = function() {
+
+    /* Create board as a 2d array - this will  allow any cell to be 
+     identified by board[column][row] */
     const board =  [["X", "O", "X"],
-                    ["O", "O", "X"],
+                    ["O", "", "X"],
                     ["X", "X", "O"]];
 
     const getBoard = () => board;
-    
-    return {board, getBoard};
+
+    const addPlayerMove = (playerSymbol, column, row) => {
+        // Check if the move is valid by confirming the cell is empty.
+        if (!board[column][row]) {
+            board[column][row] = playerSymbol;
+        }
+    }
+
+    return {board, getBoard, addPlayerMove};
 }()
+
+function createPlayer(name, symbol) {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+
+    return {getName, getSymbol};
+}
+
+function gameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+    // Create the players.
+    const playerOne = createPlayer(playerOneName, "O");
+    const playerTwo = createPlayer(playerTwoName, "X");
+
+    return {playerOne, playerTwo};
+}
